c1cf3fcabc9b630ac5fb91f926d347a59fa5a7cf
Alkhila <alkhilasyadza@gmail.com>
2025-09-27 22:08:34 +0700

    nambahin home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -30,7 +30,87 @@ class HomePage extends StatelessWidget {
       ),
 
       body: Center(
-        child: Image.asset('assets/banner.jpg', width: 200, height: 200),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/banner.jpg',
+              height: 200,
+              width: double.infinity, // Membuat gambar memenuhi lebar layar
+              fit: BoxFit
+                  .cover, // Memastikan gambar memenuhi area yang ditentukan tanpa distorsi
+            ),
+            const SizedBox(height: 30),
+            Text(
+              'Daftar Menu',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 11,
+                  children: <Widget>[
+                    // Memanggil widget _menuItem() secara manual untuk setiap menu
+                    _MenuMakanan(
+                      nama: "Nasi Goreng",
+                      harga: "Rp 15000",
+                      foto:
+                          "assets/logo_burger.png", // Ganti dengan path gambar Anda
+                    ),
+                    _MenuMakanan(
+                      nama: "Mie Ayam",
+                      harga: "Rp 12000",
+                      foto: "assets/logo_burger.png",
+                    ),
+                    _MenuMakanan(
+                      nama: "Sate Ayam",
+                      harga: "Rp 20000",
+                      foto: "assets/logo_burger.png",
+                    ),
+                    _MenuMakanan(
+                      nama: "Bakso",
+                      harga: "Rp 10000",
+                      foto: "assets/logo_burger.png",
+                    ),
+                    // Tambahkan menu lainnya di sini
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _MenuMakanan({required nama, required harga, required foto}) {
+    return Container(
+      child: Card(
+        margin: EdgeInsets.all(8),
+        elevation: 6, // Beri efek bayangan
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15), // Atur sudut melengkung
+        ),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(foto, width: 100, height: 100),
+            ),
+            Text(nama, style: TextStyle(fontWeight: FontWeight.bold)),
+            Text("Harga: $harga", style: TextStyle()),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Pesan", style: TextStyle(color: Colors.white)),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 150, 109, 88),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
